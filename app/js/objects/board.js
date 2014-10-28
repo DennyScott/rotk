149f4c7fc5309470cf0c8d464edbea52bd5e789c
Denny Scott <dennyscott301@gmail.com>
2014-10-27 23:58:32 -0500

    Added Numbers to Tiles

--- app/js/objects/board.js
+++ app/js/objects/board.js
@@ -4,19 +4,47 @@
 		var _board = this;
 		var _completeBoard = game.add.group();
 		var _outerBoard;
-		var _one, _two, _three, _four, _five, _six, _seven, _eight, _nine;
+		var _one = {
+				text: '1'
+			},
+			_two = {
+				text: '2'
+			},
+			_three = {
+				text: '3'
+			},
+			_four = {
+				text: '4'
+			},
+			_five = {
+				text: '5'
+			},
+			_six = {
+				text: '6'
+			},
+			_seven = {
+				text: '7'
+			},
+			_eight = {
+				text: '8'
+			},
+			_nine = {
+				text: '9'
+			};
 		var _scale = 0.333333;
 		var _positions;
 		var _tileArray;
+		var text = game.add.group(_completeBoard, 'text', true);
 		var tiles = game.add.group(_completeBoard, 'tiles', true);
 
-		var _initalize = function(){
-			_outerBoard = game.add.image(game.world.centerX, game.world.centerY, 'blackTile');
+		var _initalize = function() {
+			_outerBoard = game.add.sprite(game.world.centerX, game.world.centerY, 'blackTile');
 			_outerBoard.anchor.setTo(0.5, 0.5);
 
 			_getPositions();
 			_createTileArray();
 			_createTilesOnBoard();
+			_rotateBoard();
 		};
 
 		var _createTileArray = function() {
@@ -25,34 +53,66 @@
 
 		var _createTilesOnBoard = function() {
 			for (var i = 0; i < _tileArray.length; i++) {
-				_tileArray[i] = game.add.image(_positions[i].x, _positions[i].y, 'whiteTile', null, tiles);
-				_tileArray[i].scale.x = _scale;
-				_tileArray[i].scale.y = _scale;
-			};
+				_tileArray[i].sprite = game.add.sprite(_positions[i].x, _positions[i].y, 'whiteTile', null, tiles);
+				_tileArray[i].sprite.scale.x = _scale;
+				_tileArray[i].sprite.scale.y = _scale;
+				_outerBoard.addChild(_tileArray[i].sprite);
 
-			
+				var spriteText = game.add.text(game.world.centerX - (_tileArray[i].sprite.width / 2), game.world.centerY - (_tileArray[i].sprite.height / 2), _tileArray[i].text, 
+				{
+					font: '70px Geo',
+					fill: '#000000',
+					align: 'center'
+				}, text);
+
+				spriteText.anchor.setTo(0.5, 0.5);
+				spriteText.angle = -45;
+
+				_tileArray[i].sprite.addChild(spriteText);
+			};
+		};
+
+		var _rotateBoard = function() {
+			_outerBoard.angle += 45;
 		}
 
 		var _getPositions = function() {
-			var xLeft = _outerBoard.x - (_outerBoard.width/2);
-			var xCenter = xLeft + (_outerBoard.width*_scale);
-			var xRight = _outerBoard.x + (_outerBoard.width/2) - (_outerBoard.width*_scale);
+			var xLeft = 0 - (_outerBoard.width / 2);
+			var xCenter = xLeft + (_outerBoard.width * _scale);
+			var xRight = 0 + (_outerBoard.width / 2) - (_outerBoard.width * _scale);
 
-			var yTop = _outerBoard.y - (_outerBoard.height/2);
-			var yCenter = yTop + (_outerBoard.height*_scale);
-			var yBottom = _outerBoard.y + (_outerBoard.height/2) - (_outerBoard.height * _scale);
+			var yTop = 0 - (_outerBoard.height / 2);
+			var yCenter = yTop + (_outerBoard.height * _scale);
+			var yBottom = 0 + (_outerBoard.height / 2) - (_outerBoard.height * _scale);
 
-			_positions = [
-				{x: xLeft, y: yTop},
-				{x: xCenter, y: yTop},
-				{x: xRight, y: yTop},
-				{x: xLeft, y: yCenter},
-				{x: xCenter, y: yCenter},
-				{x: xRight, y: yCenter},
-				{x: xLeft, y: yBottom},
-				{x: xCenter, y: yBottom},
-				{x: xRight, y: yBottom}
-			];
+			_positions = [{
+				x: xLeft,
+				y: yTop
+			}, {
+				x: xCenter,
+				y: yTop
+			}, {
+				x: xRight,
+				y: yTop
+			}, {
+				x: xLeft,
+				y: yCenter
+			}, {
+				x: xCenter,
+				y: yCenter
+			}, {
+				x: xRight,
+				y: yCenter
+			}, {
+				x: xLeft,
+				y: yBottom
+			}, {
+				x: xCenter,
+				y: yBottom
+			}, {
+				x: xRight,
+				y: yBottom
+			}];
 		};
 
 		_initalize();

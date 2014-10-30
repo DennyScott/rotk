e6f8bdb414242cda23ba44fbed39252c089a6035
Denny Scott <dennyscott301@gmail.com>
2014-10-29 21:28:30 -0500

    Added player documentation

--- app/js/objects/player.js
+++ app/js/objects/player.js
@@ -1,28 +1,51 @@
 (function() {
 
+	/**
+	 * Player Class
+	 */
 	var Player = function(name, x, y){
 		var _player = this;
-		var _name;
-		var _health;
-		var _text;
+		var _name; //Players Name
+		var _health; //Players Health
+		var _text; //The Text Object to display
 
+		/**
+		 * Constructor
+		 * @param  {String} name Players Name
+		 * @param  {int} x    The X coordinate to place text at
+		 * @param  {int} y    The Y coordinate to place text at
+		 */
 		var _initalize = function(name, x, y){
 			_name = name;
 			_health = 50;
-			_text = game.add.text(x, y, _createLabel(), {
+			
+			//Create Text Object_
+			text = game.add.text(x, y, _createLabel(), {
 				font: '40px Geo',
 				fill: '#000000'
 			})
 		};
 
+		/**
+		 * Generate the label used to display the players current health
+		 * @return {String} Users current health
+		 */
 		var _createLabel = function(){
 			return name + "'s Health: " + _health;
 		}
 
+		/**
+		 * Get Players Health
+		 * @return {int} Current Health
+		 */
 		this.health = function(){
 			return _health;
 		}
 
+		/**
+		 * Is the player at or below 0 health.
+		 * @return {Boolean} Is the player at or below 0 health.
+		 */
 		this.isDead = function(){
 			if(_health <= 0 ){
 				return true
@@ -31,30 +54,52 @@
 			}
 		}
 
+		/**
+		 * Get the width of the _text object.
+		 * @return {int} Current width of the object
+		 */
 		this.getWidth = function() {
 			return _text.width;
 		}
 
+		/**
+		 * Get the Height of the _text object
+		 * @return {[type]} [description]
+		 */
 		this.getHeight = function() {
 			return _text.height;
 		}
 
+		/**
+		 * Player takes damage, reducing the number from the
+		 * health, then displaying.
+		 * 
+		 * @param  {int} amount Damage to reduce from health.
+		 * @return {int}        Return current health after damage.
+		 */
 		this.takeDamage = function(amount){
 			_health -= amount;
-			_text.text = _createLabel();
+			_text.text = _createLabel(); //Display Current Health
 			return _health;
 		}
 
+		/**
+		 * Player recieves health, increasing their health by the number
+		 * passed.
+		 * 
+		 * @param  {int} amount Health to increase to health.
+		 * @return {int}        Return current health after heal.
+		 */
 		this.heal = function(amount){
 			_health += amount;
-			_text.text = _createLabel();
+			_text.text = _createLabel(); //Display current health
 			return _health;
 		}
 
 
-		_initalize(name, x, y);
+		_initalize(name, x, y); //Call constructor
 	}
 
-	game.player = Player;
+	game.player = Player; //Attach to global scope
 
 })();

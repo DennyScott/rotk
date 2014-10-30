df03421f2b99fa8f76431bc9c711091c269c2b5f
Denny Scott <dennyscott301@gmail.com>
2014-10-29 21:18:55 -0500

    Added Player Class

--- app/js/objects/player.js
+++ app/js/objects/player.js
@@ -1,16 +1,58 @@
 (function() {
 
-	var Player = function(name){
+	var Player = function(name, x, y){
+		var _player = this;
 		var _name;
 		var _health;
+		var _text;
 
-		var _initalize = function(name){
+		var _initalize = function(name, x, y){
 			_name = name;
 			_health = 50;
+			_text = game.add.text(x, y, _createLabel(), {
+				font: '40px Geo',
+				fill: '#000000'
+			})
 		};
 
+		var _createLabel = function(){
+			return name + "'s Health: " + _health;
+		}
 
-		_initalize(name);
+		this.health = function(){
+			return _health;
+		}
+
+		this.isDead = function(){
+			if(_health <= 0 ){
+				return true
+			}else{
+				return false;
+			}
+		}
+
+		this.getWidth = function() {
+			return _text.width;
+		}
+
+		this.getHeight = function() {
+			return _text.height;
+		}
+
+		this.takeDamage = function(amount){
+			_health -= amount;
+			_text.text = _createLabel();
+			return _health;
+		}
+
+		this.heal = function(amount){
+			_health += amount;
+			_text.text = _createLabel();
+			return _health;
+		}
+
+
+		_initalize(name, x, y);
 	}
 
 	game.player = Player;

6a20a34a5f9f0e2b8999d3d85fb5c955a658e887
Gizmmo <travisscott301@gmail.com>
2014-10-31 21:46:10 -0500

    Player creation extracted to the menu, createView added to create needed score tags

--- app/js/objects/player.js
+++ app/js/objects/player.js
@@ -3,7 +3,7 @@
 	/**
 	 * Player Class
 	 */
-	var Player = function(name, x, y){
+	var Player = function(name) {
 		var _player = this;
 		var _name; //Players Name
 		var _health; //Players Health
@@ -15,38 +15,41 @@
 		 * @param  {int} x    The X coordinate to place text at
 		 * @param  {int} y    The Y coordinate to place text at
 		 */
-		var _initalize = function(name, x, y){
+		var _initalize = function(name) {
 			_name = name;
 			_health = 50;
-			
-			//Create Text Object
-			_text = game.add.text(x, y, _createLabel(), {
-				font: '40px Geo',
-				fill: '#000000'
-			})
+
 		};
 
 		/**
 		 * Generate the label used to display the players current health
 		 * @return {String} Users current health
 		 */
-		var _createLabel = function(){
+		var _createLabel = function() {
 			return name + "'s Health: " + _health;
+		}
+
+		this.createView = function(x, y) {
+			//Create Text Object
+			_text = game.add.text(x, y, _createLabel(), {
+				font: '40px Geo',
+				fill: '#000000'
+			});
 		}
 
 		/**
 		 * Check if the player is currently holding a defence card. The defence card
 		 * cannot be in the procces of 'being played' this turn.
-		 * 
+		 *
 		 * @return {Boolean} Player is holding defence card?
 		 */
-		this.isHoldingDefenceCard = function(){
+		this.isHoldingDefenceCard = function() {
 			//Must check if the player played this defence card this turn
 		},
 
 		/**
 		 * Use the defence card that the player is currently holding.
-		 * 
+		 *
 		 */
 		this.useDefenseCard = function() {
 
@@ -56,7 +59,7 @@
 		 * Get Players Health
 		 * @return {int} Current Health
 		 */
-		this.health = function(){
+		this.health = function() {
 			return _health;
 		}
 
@@ -64,10 +67,10 @@
 		 * Is the player at or below 0 health.
 		 * @return {Boolean} Is the player at or below 0 health.
 		 */
-		this.isDead = function(){
-			if(_health <= 0 ){
+		this.isDead = function() {
+			if (_health <= 0) {
 				return true
-			}else{
+			} else {
 				return false;
 			}
 		}
@@ -91,11 +94,11 @@
 		/**
 		 * Player takes damage, reducing the number from the
 		 * health, then displaying.
-		 * 
+		 *
 		 * @param  {int} amount Damage to reduce from health.
 		 * @return {int}        Return current health after damage.
 		 */
-		this.takeDamage = function(amount){
+		this.takeDamage = function(amount) {
 			_health -= amount;
 			_text.text = _createLabel(); //Display Current Health
 			return _health;
@@ -104,18 +107,18 @@
 		/**
 		 * Player recieves health, increasing their health by the number
 		 * passed.
-		 * 
+		 *
 		 * @param  {int} amount Health to increase to health.
 		 * @return {int}        Return current health after heal.
 		 */
-		this.heal = function(amount){
+		this.heal = function(amount) {
 			_health += amount;
 			_text.text = _createLabel(); //Display current health
 			return _health;
 		}
 
 
-		_initalize(name, x, y); //Call constructor
+		_initalize(name); //Call constructor
 	}
 
 	game.player = Player; //Attach to global scope
--- app/js/states/play.js
+++ app/js/states/play.js
@@ -9,8 +9,8 @@ var playState = {
 	create: function() {
 		var board = new game.board(game.world.centerX, game.world.centerY, 0.5);
 		game.global.currentBoard = new game.board(game.world.centerX, game.world.centerY, 0.5);
-		game.global.playerOne = new game.player('Denny', 0, 10);
-		game.global.playerTwo = new game.player('Travis', 0, game.global.playerOne.getHeight() + 20);
+		game.global.playerOne.createView(0, 10);
+		game.global.playerTwo.createView(0, game.global.playerOne.getHeight() + 20);
 		game.global.arrow = new game.arrow(game.world.centerX + 200,
 			0, .5);
 

66f9ea980269e9c809445e8388aabd78464b4fd0
Denny Scott <dennyscott301@gmail.com>
2014-11-02 15:50:55 -0600

    Added Defence Counter Animation

--- app/js/actions/animations.js
+++ app/js/actions/animations.js
@@ -5,13 +5,15 @@ game.animations = {
 	showCards: function(oneCard, twoCard) {
 		this.clearBoard();
 
+		this.setAlpha = 1;
+
 		if (typeof oneCard !== 'undefined') {
 			this.oneCard = oneCard;
 			this.oneCard.createView(0, 0);
 			this.createCard(oneCard, {
-				x: game.world.centerX - oneCard.view().width, 
-				y: game.world.height - oneCard.view().height, 
-				scale: 0.75, 
+				x: game.world.centerX - oneCard.view().width,
+				y: game.world.height - oneCard.view().height,
+				scale: 0.75,
 				fromTween: -20
 			});
 		}
@@ -20,9 +22,9 @@ game.animations = {
 			this.twoCard = twoCard;
 			this.twoCard.createView(0, 0);
 			this.createCard(twoCard, {
-				x: game.world.centerX + twoCard.view().width, 
-				y: game.world.height - twoCard.view().height, 
-				scale: 0.75, 
+				x: game.world.centerX + twoCard.view().width,
+				y: game.world.height - twoCard.view().height,
+				scale: 0.75,
 				fromTween: 20
 			});
 		}
@@ -37,7 +39,7 @@ game.animations = {
 		game.add.tween(card.view()).to({
 			alpha: 1,
 			x: options.x
-		}, 800, Phaser.Easing.Linear.None, true, 1000);
+		}, 800, Phaser.Easing.Linear.None, true, 1500);
 	},
 
 	clearBoard: function() {
@@ -47,19 +49,25 @@ game.animations = {
 
 	cardWinner: function(winner, loser) {
 		loser = this.checkCardsExist(loser);
-		var tween = game.add.tween(loser.view()).to({
-			alpha: 0,
-			y: loser.view().y - 20
-		}, 1200, Phaser.Easing.Linear.None, true, 800);
-		tween.onComplete.add(this.removeCards, this);
+		var _this = this;
+		setTimeout(function() {
+			loser.view().alpha = _this.setAlpha;
+			var tween = game.add.tween(loser.view()).to({
+				alpha: 0,
+				y: loser.view().y - 20
+			}, 1200, Phaser.Easing.Linear.None, true, 800);
+			tween.onComplete.add(_this.removeCards, _this);
+		}, 1200)
+
 	},
 
 	checkCardsExist: function(card) {
-			if (typeof card === 'undefined') {
-				return this.createTempView();
-			}
+		if (typeof card === 'undefined') {
+			this.setAlpha = 0;
+			return this.createTempView();
+		}
 
-			return card;
+		return card;
 	},
 
 	createTempView: function() {
@@ -74,7 +82,7 @@ game.animations = {
 
 	removeCards: function() {
 		this.oneCard = this.checkCardsExist(this.oneCard);
-		this.twoCard =this.checkCardsExist(this.twoCard);
+		this.twoCard = this.checkCardsExist(this.twoCard);
 		game.add.tween(this.twoCard.view()).to({
 			alpha: 0,
 		}, 600, Phaser.Easing.Linear.None, true, 600);
@@ -92,6 +100,49 @@ game.animations = {
 		this.twoCard.view().destroy();
 		this.oneCard = undefined;
 		this.twoCard = undefined;
+	},
+
+	defenceCounter: function(card, defenceCard) {
+		this.setAlpha = 0; //Set the Alpha to 0, so the old loser card will disappear
+		var cView = card.view();
+		defenceCard.createView(cView.x, cView.y);
+		var dView = defenceCard.view();
+		dView.scale.x = dView.scale.y = cView.scale.x;
+		dView.alpha = 1;
+
+		//Hide the view of the loser card
+		if (card.owner === this.oneCard.owner) {
+			this.oneCard.view().alpha = 0;
+		} else {
+			this.twoCard.view().alpha = 0;
+		}
+
+		this.animateDefenceCard(dView); //Perform Animations for Defence Couhnter
+
+
+	},
+
+	animateDefenceCard: function(dView) {
+		game.add.tween(dView.scale).to({
+			x: dView.scale.x + .1,
+			y: dView.scale.x + .1,
+		}, 300, Phaser.Easing.Linear.None, true, 50)
+			.to({
+				x: dView.scale.x - .2,
+				y: dView.scale.x - .2,
+			}, 300, Phaser.Easing.Linear.None, true, 50)
+			.to({
+				x: dView.scale.x + .15,
+				y: dView.scale.x + .15,
+			}, 200, Phaser.Easing.Linear.None, true, 50)
+			.to({
+				x: dView.scale.x - .05,
+				y: dView.scale.x - .05,
+			}, 100, Phaser.Easing.Linear.None, true, 50);
+
+		game.add.tween(dView).to({
+			alpha: 0
+		}, 2500, Phaser.Easing.Linear.None, true, 1400);
 	},
 
 	cardsEqual: function(oneCard, twoCard) {
